f7e4bfbc6def3033c866c93ec265b4ecead67d7f
rdrocca <rdrocca.dev@gmail.com>
2021-09-27 15:24:30 -0500

    SliderAppBar added for wh4stapp project

--- lib/whats4pp/home_whats4pp.dart
+++ lib/whats4pp/home_whats4pp.dart
@@ -15,11 +15,19 @@ class HomeWhats4pp extends StatefulWidget {
 class _HomeWhats4ppState extends State<HomeWhats4pp>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
+  late int _tabIndex;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 4, vsync: this, initialIndex: 1);
+    _tabIndex = 1;
+    _tabController =
+        TabController(length: 4, vsync: this, initialIndex: _tabIndex);
+    _tabController.addListener(() {
+      setState(() {
+        _tabIndex = _tabController.index;
+      });
+    });
   }
 
   @override
@@ -30,45 +38,58 @@ class _HomeWhats4ppState extends State<HomeWhats4pp>
 
   @override
   Widget build(BuildContext context) {
+    final showHeaders = true;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Whats4pp"),
-        bottom: TabBar(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              title: Text(
+                "Whats4pp",
+              ),
+              centerTitle: true,
+              pinned: _tabIndex == 0 ? false : showHeaders,
+              floating: showHeaders,
+              bottom: TabBar(
+                controller: _tabController,
+                tabs: [
+                  Tab(icon: Icon(Icons.camera_alt_rounded)),
+                  Tab(text: "CHATS"),
+                  Tab(text: "STATUS"),
+                  Tab(text: "CALLS"),
+                ],
+              ),
+              actions: [
+                IconButton(
+                  padding: EdgeInsets.all(12.0),
+                  icon: Icon(Icons.search),
+                  tooltip: 'Search',
+                  onPressed: () {
+                    // handle the press
+                  },
+                ),
+                IconButton(
+                  padding: EdgeInsets.all(12.0),
+                  icon: Icon(Icons.more_vert),
+                  tooltip: 'More options',
+                  onPressed: () {
+                    // handle the press
+                  },
+                )
+              ],
+            ),
+          ];
+        },
+        body: TabBarView(
+          physics: BouncingScrollPhysics(),
           controller: _tabController,
-          tabs: [
-            Tab(icon: Icon(Icons.camera_alt_rounded)),
-            Tab(text: "CHATS"),
-            Tab(text: "STATUS"),
-            Tab(text: "CALLS"),
+          children: [
+            CameraTab(),
+            ChatsTab(),
+            StatusTab(),
+            CallsTab(),
           ],
         ),
-        actions: [
-          IconButton(
-            padding: EdgeInsets.all(12.0),
-            icon: Icon(Icons.search),
-            tooltip: 'Search',
-            onPressed: () {
-              // handle the press
-            },
-          ),
-          IconButton(
-            padding: EdgeInsets.all(12.0),
-            icon: Icon(Icons.more_vert),
-            tooltip: 'More options',
-            onPressed: () {
-              // handle the press
-            },
-          )
-        ],
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-          CameraTab(),
-          ChatsTab(),
-          StatusTab(),
-          CallsTab(),
-        ],
       ),
     );
   }
